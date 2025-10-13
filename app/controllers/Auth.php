31804525bc34c9e163a0ff522c909e467c433a3d
Giovanny Orozco Quintero <giovannyorozco4000@gmail.com>
2025-10-12 19:05:49 -0500

    Agregar ejemplo de metodo con la clase Usuario

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -3,6 +3,8 @@ require_once __DIR__ . '/../models/Usuario.php';
 
 class Auth {
   public static function login(Router $Router, array $params): void {
+   
+    echo Usuario::saludar('Giovanny');
     
   }
 }
--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -1,4 +1,10 @@
 <?php
 class Usuario extends DB {
   protected static $tabla = 'usuarios';
+
+  public static function saludar(string $nombre): string {
+
+    return "Hola usuario $nombre, como estas?";
+
+  }
 }
