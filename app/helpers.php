5446a365b283275af85180954f0aa9cefcdd2383
Giovanny Orozco Quintero <giovannyorozco4000@gmail.com>
2025-11-10 17:27:17 -0500

    Agregar funciones (formatearMoneda, formatearFecha, limpiarFormatoMoneda)

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,14 @@ function debuguear($variable): string {
   exit;
 }
 
+function formatearMoneda(int|float|string $valor): string {
+  return number_format($valor, 0, ',', '.');
+}
+
+function limpiarFormatoMoneda(string $valor): string {
+  return str_replace('.', '', $valor);
+}
+
+function formatearFecha(string $fecha): string {
+  return date('d-m-Y', strtotime($fecha));
+}
