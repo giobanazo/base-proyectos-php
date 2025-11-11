39a0c400e73d07e1b943408664f1909b668d46b4
Giovanny Orozco Quintero <giovannyorozco4000@gmail.com>
2025-11-11 12:13:52 -0500

    Agregar el archivo API.php

---
+++ app/controllers/API.php
@@ -0,0 +1,22 @@
+<?php
+class API {
+  private static function send(string $status, string|array $response): void {
+    echo json_encode(['status' => $status, 'response' => $response]);
+    exit;
+  }
+
+  /*
+  public static function getIngresoById(Router $Router, array $params): void {
+    if (!isAuthenticated()) self::send('ERROR', 'ACCESS_DENIED');
+
+    require_once __DIR__ . '/../models/Ingreso.php';
+
+    $result = Ingreso::whereMultiple([
+      'usuario' => $_SESSION['user']['usuario'],
+      'id' => $params[0]
+    ], 'AND', ['id', 'fecha', 'concepto', 'valor'])[0];
+
+    $result ? self::send('OK', $result) : self::send('ERROR', 'NOT_FOUND');
+  }
+  */
+}
