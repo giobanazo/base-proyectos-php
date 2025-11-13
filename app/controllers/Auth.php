6c4fd4f4a720f4d1bc8fdf7377f5a6cc0874cb11
Giovanny Orozco Quintero <giovannyorozco4000@gmail.com>
2025-11-12 21:41:08 -0500

    Eliminar el metodo saludar del modelo Usuario en el controlador Auth.php

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -2,9 +2,5 @@
 require_once __DIR__ . '/../models/Usuario.php';
 
 class Auth {
-  public static function login(Router $Router, array $params): void {
-   
-    echo Usuario::saludar('Giovanny');
-    
-  }
+  public static function login(Router $Router, array $params): void {}
 }
