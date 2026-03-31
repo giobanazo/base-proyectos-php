a4208b66893f8ea2a6a10b7dd16b02b2c8404dcc
Giovanny Orozco <giovannyorozco4000@gmail.com>
2026-03-31 14:47:07 -0500

    Agregar método crearRememberToken en el servicio AuthService

--- app/services/AuthService.php
+++ app/services/AuthService.php
@@ -1,4 +1,17 @@
 <?php
 class AuthService {
-  
+  private static function crearRememberToken(int $idUsuario): void {
+    $token = bin2hex(random_bytes(32));
+    $oneMonthInUnix = time() + 2592000;
+
+    UserRememberToken::create([
+      'token_hash' => hash('sha256', $token),
+      'user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? '',
+      'ip_address' => $_SERVER['REMOTE_ADDR'] ?? '',
+      'expires_at' => date('Y-m-d H:i:s', $oneMonthInUnix),
+      'id_usuario' => $idUsuario
+    ]);
+
+    setearCookie('remember_token', $token, $oneMonthInUnix);
+  }
 }
