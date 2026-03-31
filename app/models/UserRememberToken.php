ae0dde8c2fe35563ed2cfd6095dc29a49e9817bb
Giovanny Orozco <giovannyorozco4000@gmail.com>
2026-03-31 14:41:28 -0500

    Agregar el método verificarToken en el modelo UserRememberToken

--- app/models/UserRememberToken.php
+++ app/models/UserRememberToken.php
@@ -2,5 +2,14 @@
 class UserRememberToken extends DB {
   protected static $tabla = 'user_remember_tokens';
 
+  public static function verificarToken(string $tokenHash): array|null {
+    $resultado = self::query('
+      SELECT usuarios.id, usuario, nombre, email 
+      FROM user_remember_tokens
+      INNER JOIN usuarios ON user_remember_tokens.id_usuario = usuarios.id 
+      WHERE token_hash = ? AND expires_at > NOW()', [$tokenHash]
+    )->fetch_assoc();
 
+    return $resultado;
+  }
 }
