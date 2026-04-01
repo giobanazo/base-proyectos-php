feb9a16c0e6391ef6800478e224b690cb1d5801f
Giovanny Orozco <giovannyorozco4000@gmail.com>
2026-04-01 17:33:31 -0500

    Cambiar el nombre del controlador Home por HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../models/Usuario.php';
 
-class Home {
+class HomeController {
   public static function index(Router $Router, array $params): void {
     $Router->render('inicio', ['titulo' => 'Inicio', 'page' => 'inicio']);
   }
