8576dc32dd92a467e9344454d7695a74c9f092ba
Giovanny Orozco <giovannyorozco4000@gmail.com>
2026-04-02 21:46:01 -0500

    Agregar validación requireAuth en la vista inicio

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,10 +1,11 @@
 <?php
 declare(strict_types=1);
-
+require_once __DIR__ . '/../services/AuthService.php';
 require_once __DIR__ . '/../models/Usuario.php';
 
 class HomeController {
   public static function index(Router $Router, array $params): void {
+    AuthService::requireAuth();
     $Router->render('inicio', ['titulo' => 'Inicio', 'page' => 'inicio']);
   }
 }
