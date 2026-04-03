b24fb5173e268cb7c3efe2aead9e568fcafce9f2
Giovanny Orozco <giovannyorozco4000@gmail.com>
2026-04-02 21:37:10 -0500

    Agregar validación de requireGuest en el apartado del login

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '/../models/Usuario.php';
 
 class AuthController {
   public static function login(Router $Router, array $params): void {
+    AuthService::requireGuest();
     $Router->render('pages/login', [], false);
   }
 
