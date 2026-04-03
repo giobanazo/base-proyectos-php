fce78b68b1c3f2190477c191a7aba2d05f054fd8
Giovanny Orozco <giovannyorozco4000@gmail.com>
2026-04-03 18:05:55 -0500

    Eliminar require_once de los modelos a los controladores

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -2,8 +2,6 @@
 declare(strict_types=1);
 require_once __DIR__ . '/../services/AuthService.php';
 
-require_once __DIR__ . '/../models/Usuario.php';
-
 class AuthController {
   public static function login(Router $Router, array $params): void {
     AuthService::requireGuest();
--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 require_once __DIR__ . '/../services/AuthService.php';
-require_once __DIR__ . '/../models/Usuario.php';
 
 class HomeController {
   public static function index(Router $Router, array $params): void {
