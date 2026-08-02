dee5b6364fb793945e86d7ec87e499e4588c53d8
Giovanny Orozco <giovannyorozco4000@gmail.com>
2026-08-02 12:14:31 -0500

    refactor: Tipar con never el retorno de send() en APIController.php

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 class APIController {
-  private static function send(string $status, string|array $response): void {
+  private static function send(string $status, string|array $response): never {
     echo json_encode(['status' => $status, 'response' => $response]);
     exit;
   }
